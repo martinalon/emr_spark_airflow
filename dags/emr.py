a0ae744ff65ac771321a9e47597f05a9df8d413b
martinalon <martinalonsofloresgonzalez@gmail.com>
2022-08-06 19:11:13 -0500

    ok

--- dags/emr.py
+++ dags/emr.py
@@ -6,7 +6,7 @@ from airflow.providers.amazon.aws.operators.emr_create_job_flow import (
 )
 from airflow.providers.amazon.aws.sensors.emr_job_flow import EmrJobFlowSensor
 from airflow.utils.dates import days_ago
-from pip.operations import freeze
+#from pip.operations import freeze
 
 DEFAULT_ARGS = {
     "owner": "airflow",
